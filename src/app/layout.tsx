6294c219c2497dc583647fac7c94cfbef63ec15b
zengzilin <zengzilin@users.noreply.github.com>
2024-10-26 17:46:40 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Layout } from '@/components/Layout'
 import { Metadata } from 'next'
+import { GoogleTagManager } from '@next/third-parties/google'
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +23,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <head><Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossOrigin="anonymous" /><GoogleTagManager gtmId="G-YKVZ7690ZX" /></head>
       <body className={inter.className}>
         <Layout>{children}</Layout>
       </body>
