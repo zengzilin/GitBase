4d75e4ce5fec5bd4b2703b5b506136de9533ccb8
zengzilin <zengzilin@users.noreply.github.com>
2025-06-30 11:34:28 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Layout } from '@/components/Layout'
 import { Metadata } from 'next'
 import { GoogleTagManager } from '@next/third-parties/google'
 import Script from "next/script";
-import Head from 'next/head';
+// import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +15,16 @@ export const metadata: Metadata = {
     template: '%s | GitBase'
   },
   description: 'Open source dynamic website without database, built with Next.js and GitHub API',
+  metadataBase: new URL('https://aisphereguide.com'), // 替换为你的网站URL
+  // 添加 YeahPromos 验证元标签到 metadata 对象中
+  alternates: {
+    canonical: '/',
+  },
+  // 在这里添加自定义元标签
+  meta: [
+    { name: 'verify-yeahpromos', content: 'e25b8789a791' },
+    { name: 'google-site-verification', content: 'your-google-verification-code' }, // 示例: Google 网站验证
+  ],
 }
 
 interface RootLayoutProps {
@@ -24,13 +34,10 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <head>
-         {/* 添加 YeahPromos 验证元标签 */}
-        <meta name="verify-yeahpromos" content="e25b8789a791" />
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossOrigin="anonymous" /><GoogleTagManager gtmId="G-YKVZ7690ZX" />
-      </head>
       <body className={inter.className}>
         <Layout>{children}</Layout>
+        {/* 添加 YeahPromos 验证元标签 */}
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossOrigin="anonymous" /><GoogleTagManager gtmId="G-YKVZ7690ZX" />
       </body>
     </html>
   )
