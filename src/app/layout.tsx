696b0ad0e0ee1880a12fe10f29ba929f3d1f2e05
zengzilin <zengzilin@users.noreply.github.com>
2025-06-30 10:10:51 +0800

    Update layout.tsx
     {/* 添加 YeahPromos 验证元标签 */}
            <meta name="verify-yeahpromos" content="e25b8789a791" />

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Layout } from '@/components/Layout'
 import { Metadata } from 'next'
 import { GoogleTagManager } from '@next/third-parties/google'
 import Script from "next/script";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,7 +24,11 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <head><Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossOrigin="anonymous" /><GoogleTagManager gtmId="G-YKVZ7690ZX" /></head>
+      <head>
+         {/* 添加 YeahPromos 验证元标签 */}
+        <meta name="verify-yeahpromos" content="e25b8789a791" />
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossOrigin="anonymous" /><GoogleTagManager gtmId="G-YKVZ7690ZX" />
+      </head>
       <body className={inter.className}>
         <Layout>{children}</Layout>
       </body>
