5e4a67ee740ccde5f31f5c2d950ae38da7195d7e
zengzilin <zengzilin@users.noreply.github.com>
2025-06-30 13:30:29 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,15 +15,9 @@ export const metadata: Metadata = {
     template: '%s | GitBase'
   },
   description: 'Open source dynamic website without database, built with Next.js and GitHub API',
-  metadataBase: new URL('https://gitbase.com'), // 替换为你的网站URL
-  alternates: {
-    canonical: '/',
-  },
-  // 使用其他官方支持的元数据字段或 other 属性
-  other: {
-    'verify-yeahpromos': 'e25b8789a791',
-    // 其他自定义元标签...
-  }
+   meta: [ 
+        { name: 'verify-yeahpromos', content: 'e25b8789a791' }, 
+     ],
 }
 
 interface RootLayoutProps {
@@ -35,8 +29,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en">
       <body className={inter.className}>
         <Layout>{children}</Layout>
-         <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossOrigin="anonymous" />
-        <GoogleTagManager gtmId="G-YKVZ7690ZX" />
       </body>
     </html>
   )
