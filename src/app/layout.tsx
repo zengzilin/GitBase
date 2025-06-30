4bd51e28c2676492b23389cb4a551763e54ab195
zengzilin <zengzilin@users.noreply.github.com>
2025-06-30 13:36:16 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] })
 
+// 扩展 Metadata 类型以包含 meta 属性 
+type CustomMetadata = Metadata & { 
+  meta?: { name: string; content: string }[]; 
+}; 
 
 export const metadata: Metadata = {
   title: {
@@ -17,8 +21,8 @@ export const metadata: Metadata = {
   description: 'Open source dynamic website without database, built with Next.js and GitHub API',
    meta: [ 
         { name: 'verify-yeahpromos', content: 'e25b8789a791' }, 
-     ],
-}
+     ]
+};
 
 interface RootLayoutProps {
   children: React.ReactNode
